9fe0afeca0e83c7b48437ba7ddca11479c353319
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-02 09:35:24 +0700

    add parameter in function

--- week5/function.php
+++ week5/function.php
@@ -1,12 +1,16 @@
 <?php
-
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Sherly<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+//membuat fungsi
+function perkenalan($nama, $salam){
+echo $salam.", ";
+echo "Perkenalkan, nama saya ".$nama."<br/>"; 
+echo "Senang berkenalan dengan Anda<br/>";
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+//memanggil fungsi yang sudah dibuat 
+perkenalan("Hamdana", "Hallo");
+echo "<hr>";
+$saya = "Sherly";
+$ucapanSalam = "Selamat pagi";
+//memanggil lagi 
+perkenalan ($saya, $ucapanSalam); 
 ?>
