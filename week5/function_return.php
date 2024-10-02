05de1a4e44caa2aa367463f026e393a0c3a960fc
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-02 18:30:08 +0700

    initial commit

---
+++ week5/function_return.php
@@ -0,0 +1,10 @@
+<?php
+//membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur (1988, 2023). " tahun" 
+// isi sesuai dengan tahun lahir 
+?>
