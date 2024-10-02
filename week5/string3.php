092bf1c19861ea9773bcc57a4c59a5a125f071ef
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-02 20:27:32 +0700

    reverse a string word by word

--- week5/string3.php
+++ week5/string3.php
@@ -1,6 +1,10 @@
 <?php
-
-$pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
-
+$pesan = "saya arek malang";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerkata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerkata);
+#gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+echo $pesan . "<br>";
 ?>
