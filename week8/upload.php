bfa495f6527c8f8cb312aadabe73149639914a7a
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-17 09:06:26 +0700

    add extension and maxsize

--- week8/upload.php
+++ week8/upload.php
@@ -2,11 +2,19 @@
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/"; // Directory where the file will be saved
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diunggah.";
+    $allowedExtension = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtension) && $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
     } else {
-        echo "Gagal mengunggah file.";
+        echo "File tidak valid atau melebihi ukuran maximum yang diizinkan.";
     }
 }
 ?>
