c4c6185632dc6acbbe9220e6dda203bceb3293cc
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-17 09:24:38 +0700

    modify extension and maxsize

--- week8/upload.php
+++ week8/upload.php
@@ -4,14 +4,15 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtension = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+    //$allowedExtension = array("jpg", "jpeg", "png", "gif");
+    $allowedExtension = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
 
     if (in_array($fileType, $allowedExtension) && $_FILES["myfile"]["size"]<=$maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
             // This part displays the image thumbnail after a successful upload.
-            echo "<br><img src='$targetfile' width='200'>"; 
+            // echo "<br><img src='$targetfile' width='200'>"; 
         } else {
             echo "Gagal mengunggah file.";
         }
