a0371f5ea078078002fbd30044f93a46522076b1
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-11-15 15:54:33 +0700

    initial commit

---
+++ week11/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.min.js" integrity="sha384-oGmhGzHj/+hVXYtK4uXlSxLQoVOkBZY2g0X4B5LtOhaUEOFEsp+fK0z16Ul9+dOe" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+
+</html>
